76ec6b9578d0e36419488511540ff3a594c3524a
Andrew Zulaybar <andrewzulaybar@gmail.com>
2019-08-18 23:20:40 -0700

    Added GET method at /api/accounts to retrieve all accounts.

--- server/database/accounts.js
+++ server/database/accounts.js
@@ -8,9 +8,9 @@ const collectionName = 'usersItems';
 /**
  * Adds item credentials to user's list of items.
  *
- * @param userID - The ID of the user to add the item to.
- * @param itemID - The item identifier.
- * @param accessToken - The access token for the item.
+ * @param {number} userID - The ID of the user to add the item to.
+ * @param {string} itemID - The item identifier.
+ * @param {string} accessToken - The access token for the item.
  * @returns {Promise<Object>} - An object containing the item ID and access token.
  */
 async function addItem(userID, itemID, accessToken) {
@@ -37,6 +37,30 @@ async function addItem(userID, itemID, accessToken) {
   return newItem;
 }
 
+/**
+ * Retrieves all items associated with the given user ID.
+ *
+ * @param {number} userID - The ID of the user to search items for.
+ * @returns {Promise<Array>} - An array containing objects with the item ID and access token.
+ */
+async function getItems(userID) {
+  const items = await db.collection(collectionName).aggregate(
+    [
+      {
+        $match: { userID : userID }
+      },
+      {
+        $project: {
+          _id: 0,
+          items: 1
+        }
+      }
+    ]
+  ).toArray();
+  return items[0].items;
+}
+
 module.exports = {
-  addItem
+  addItem,
+  getItems,
 };
--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const plaid = require('plaid');
 
-const { addItem } = require('../database/accounts');
+const { addItem, getItems } = require('../database/accounts');
 
 const router = express.Router();
 const client = new plaid.Client(
@@ -19,21 +19,60 @@ router.post('/get_access_token', (req, res) => {
   client.exchangePublicToken(
     req.body.public_token,
     async (error, tokenResponse) => {
-      if (error !== null) {
-        const msg = 'Could not exchange public_token!';
-        console.log(msg + '\n' + error);
-        return res.json({ error: msg });
-      }
+      if (error)
+        console.log(error);
 
       const itemID = tokenResponse.item_id;
       const accessToken = tokenResponse.access_token;
-
       await addItem(1, itemID, accessToken);
-      console.log('Item ID: ' + itemID);
-      console.log('Access Token: ' + accessToken);
-      res.json({ error: false });
+      return res.json({ error: false });
     }
   );
 });
+
+/**
+ * GET all accounts, across all items.
+ */
+router.get('/', (req, res) => {
+  const accounts = [];
+  getItems(1)
+    .then(async items => {
+      const promises = [];
+      for (let i = 0; i < items.length; i++) {
+        promises.push(
+          getAccounts(items[i])
+          .then(account => accounts.push(account))
+          .catch(error => console.log(error))
+        );
+      }
+      await Promise.all(promises);
+      res.send({ accounts: accounts });
+    })
+    .catch(error => console.log(error));
+});
+
+/**
+ * Retrieves all accounts associated with the given item.
+ *
+ * @param {object} item - The item ID and access token that identifies the item.
+ * @returns {Promise<Object>} - Detailed information of the accounts associated with the item.
+ */
+async function getAccounts(item) {
+  let response = {};
+  await new Promise( resolve => {
+    client.getAuth(item.accessToken, {}, (error, results) => {
+      if (error)
+        return new Error();
+
+      response = {...results.accounts};
+      if (results.numbers.ach.length > 0)
+        response = {...response, ...results.numbers.ach };
+      if (results.numbers.eft.length > 0)
+        response = {...response, ...results.numbers.eft };
+      resolve();
+    })
+  });
+  return response;
+}
 
 module.exports = router;
