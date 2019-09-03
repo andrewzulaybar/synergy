d91f8b642519759e361747ea91dc93cb244b53ef
Andrew Zulaybar <andrewzulaybar@gmail.com>
2019-09-03 16:19:25 -0700

    Migrated Root and Sidebar class components to function components.

--- client/src/screens/root/Root.jsx
+++ client/src/screens/root/Root.jsx
@@ -1,5 +1,5 @@
-import { Layout } from "antd";
-import React, { Component } from "react";
+import { Layout } from 'antd';
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import 'antd/dist/antd.css';
 
@@ -8,18 +8,16 @@ import Home from '../home/Home';
 import Sidebar from '../sidebar/Sidebar';
 import '../sidebar/Sidebar.css';
 
-class Root extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Layout>
-          <Sidebar />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/accounts" component={Accounts} />
-        </Layout>
-      </BrowserRouter>
-    );
-  }
-}
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Sidebar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/accounts" component={Accounts} />
+      </Layout>
+    </BrowserRouter>
+  );
+};
 
 export default Root;
--- client/src/screens/sidebar/Sidebar.jsx
+++ client/src/screens/sidebar/Sidebar.jsx
@@ -1,46 +1,38 @@
 import { Icon, Layout, Menu } from 'antd';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
 
-class Sidebar extends Component {
-  state = {
-    collapsed: false,
-  };
+const Sidebar = withRouter(({ location }) => {
+  const [collapsed, setCollapsed] = useState(false);
 
-  onCollapse = collapsed => {
-    this.setState({ collapsed })
-  };
-
-  render() {
-    return (
-      <Layout.Sider
-        breakpoint="md"
-        className="sidebar"
-        collapsible
-        collapsed={this.state.collapsed}
-        onCollapse={this.onCollapse}
-        theme="light"
+  return (
+    <Layout.Sider
+      breakpoint="md"
+      className="sidebar"
+      collapsible
+      collapsed={collapsed}
+      onCollapse={() => setCollapsed(collapsed => !collapsed)}
+      theme="light"
+    >
+      <Menu
+        mode="inline"
+        selectedKeys={[location.pathname]}
       >
-        <Menu
-          mode="inline"
-          selectedKeys={[this.props.location.pathname]}
-        >
-          <Menu.Item key="/">
-            <NavLink to="/">
-              <Icon type="home"/>
-              <span className="nav-text">Home</span>
-            </NavLink>
-          </Menu.Item>
-          <Menu.Item key="/accounts">
-            <NavLink to="/accounts">
-              <Icon type="credit-card" />
-              <span className="nav-text">Accounts</span>
-            </NavLink>
-          </Menu.Item>
-        </Menu>
-      </Layout.Sider>
-    );
-  }
-}
+        <Menu.Item key="/">
+          <NavLink to="/">
+            <Icon type="home"/>
+            <span className="nav-text">Home</span>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item key="/accounts">
+          <NavLink to="/accounts">
+            <Icon type="credit-card" />
+            <span className="nav-text">Accounts</span>
+          </NavLink>
+        </Menu.Item>
+      </Menu>
+    </Layout.Sider>
+  );
+});
 
-export default withRouter(Sidebar);
+export default Sidebar;
